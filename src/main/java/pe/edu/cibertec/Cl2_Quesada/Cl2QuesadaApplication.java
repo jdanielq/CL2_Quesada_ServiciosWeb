ba2a011bae84260a4c5a277e3aeae15674013130
jdanielq <quesadajuandaniel@gmail.com>
2023-10-28 18:46:04 -0500

    Primer commit

---
+++ src/main/java/pe/edu/cibertec/Cl2_Quesada/Cl2QuesadaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.Cl2_Quesada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl2QuesadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl2QuesadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/Cl2_Quesada/Cl2QuesadaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.Cl2_Quesada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl2QuesadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
