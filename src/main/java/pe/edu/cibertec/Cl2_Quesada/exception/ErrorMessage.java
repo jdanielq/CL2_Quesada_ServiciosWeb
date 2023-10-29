63e4016f9f0392409cddcc7663b0be1c882824a9
jdanielq <quesadajuandaniel@gmail.com>
2023-10-28 22:04:04 -0500

    Exception Correcciones

--- src/main/java/pe/edu/cibertec/Cl2_Quesada/exception/ErrorMessage.java
+++ src/main/java/pe/edu/cibertec/Cl2_Quesada/exception/ErrorMessage.java
@@ -1,4 +1,17 @@
 package pe.edu.cibertec.Cl2_Quesada.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@Getter
 public class ErrorMessage {
+
+    private Integer statusCode;
+    private Date dateError;
+    private String message;
+    private String description;
+
 }
--- src/main/java/pe/edu/cibertec/Cl2_Quesada/exception/ResourceNotFoundException.java
+++ src/main/java/pe/edu/cibertec/Cl2_Quesada/exception/ResourceNotFoundException.java
@@ -1,4 +1,8 @@
 package pe.edu.cibertec.Cl2_Quesada.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
 }
